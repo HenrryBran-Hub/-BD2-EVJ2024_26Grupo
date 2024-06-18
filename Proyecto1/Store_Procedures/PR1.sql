061939c3897924e01c38e7a1423e5e56cc04ad71
Brayan Rivas <bragiova.rivas@gmail.com>
2024-06-17 21:22:44 -0600

    SP Registrar Usuario y cambiar rol

---
+++ Proyecto1/Store_Procedures/PR1.sql
@@ -0,0 +1,62 @@
+﻿
+CREATE OR ALTER PROCEDURE proyecto1.PR1
+(
+	@FirstName NVARCHAR(MAX)
+	,@LastName NVARCHAR(MAX)
+	,@Email NVARCHAR(MAX)
+	,@DateOfBirth DATETIME2(7)
+	,@Password NVARCHAR(MAX)
+	,@Credits INT
+)
+AS
+BEGIN
+	BEGIN TRY
+		BEGIN TRAN RegistrarUsuarios
+			DECLARE @roleStudentId UNIQUEIDENTIFIER
+					,@idUserNew UNIQUEIDENTIFIER
+					,@idUser INT
+
+			SELECT @roleStudentId = Id
+			FROM proyecto1.Roles
+			WHERE RoleName = 'Student'
+
+			SET @idUserNew = NEWID()
+
+			IF NOT EXISTS (SELECT 1 FROM proyecto1.Usuarios WHERE Email = @Email)
+			BEGIN
+				INSERT INTO proyecto1.Usuarios(Id, Firstname, Lastname, Email, DateOfBirth, Password, LastChanges, EmailConfirmed)
+				VALUES(@idUserNew, @FirstName, @LastName, @Email, @DateOfBirth, @Password, GETDATE(), 1)
+
+				INSERT INTO proyecto1.UsuarioRole(RoleId, UserId, IsLatestVersion)
+				VALUES(@roleStudentId, @idUserNew, 1)
+
+				INSERT INTO proyecto1.ProfileStudent(UserId, Credits)
+				VALUES(@idUserNew, @Credits)
+
+				INSERT INTO proyecto1.TFA(UserId, Status, LastUpdate)
+				VALUES(@idUserNew, 0, GETDATE())
+
+				INSERT INTO proyecto1.Notification(UserId, Message, Date)
+				VALUES(@idUserNew, 'Usuario registrado correctamente en el sistema', GETDATE())
+
+				INSERT INTO proyecto1.HistoryLog(Date, Description)
+				VALUES(GETDATE(), 'OK - RegistrarUsuarios')
+
+				COMMIT TRAN RegistrarUsuarios
+			END
+			ELSE
+			BEGIN
+				ROLLBACK TRAN RegistrarUsuarios
+
+				INSERT INTO proyecto1.HistoryLog(Date, Description)
+				VALUES(GETDATE(), 'ERROR - RegistrarUsuarios - Ya existe un usuario con el email enviado')
+			END
+	END TRY
+	BEGIN CATCH
+		ROLLBACK TRAN RegistrarUsuarios
+
+		INSERT INTO proyecto1.HistoryLog(Date, Description)
+		VALUES(GETDATE(),  CONCAT('ERROR - RegistrarUsuarios', ERROR_MESSAGE()))
+	END CATCH
+END
+GO
---
+++ Proyecto1/Store_Procedures/PR2.sql
@@ -0,0 +1,63 @@
+﻿
+CREATE OR ALTER PROCEDURE proyecto1.PR2
+(
+	@Email NVARCHAR(MAX)
+	,@CodCourse INT
+)
+AS
+BEGIN
+	BEGIN TRY
+		BEGIN TRAN CambioRoles
+			DECLARE @idUser UNIQUEIDENTIFIER
+					,@rolIdTutor UNIQUEIDENTIFIER
+
+			IF NOT EXISTS (SELECT 1 FROM proyecto1.Usuarios WHERE Email = @Email)
+			BEGIN
+				INSERT INTO proyecto1.HistoryLog(Date, Description)
+				VALUES(GETDATE(), 'ERROR - CambioRoles - No existe un usuario con el email enviado')
+
+				RETURN 0
+			END
+
+			SELECT @idUser = Id
+			FROM proyecto1.Usuarios
+			WHERE Email = @Email
+
+			SELECT @rolIdTutor = Id
+			FROM proyecto1.Roles
+			WHERE RoleName = 'Tutor'
+
+			IF @rolIdTutor IS NULL
+			BEGIN
+				INSERT INTO proyecto1.HistoryLog(Date, Description)
+				VALUES(GETDATE(), 'ERROR - CambioRoles - No existe un rol de usuario Tutor')
+
+				RETURN 0
+			END
+
+			INSERT INTO proyecto1.UsuarioRole(RoleId, UserId, IsLatestVersion)
+			VALUES(@rolIdTutor, @idUser)
+
+			INSERT INTO proyecto1.TutorProfile(UserId, TutorCode)
+			VALUES(@idUser, 'PRUEBA')
+
+			INSERT INTO proyecto1.CourseTutor(TutorId, CourseCodCourse)
+			VALUES(@idUser, @CodCourse)
+
+			INSERT INTO proyecto1.Notification(UserId, Message, Date)
+			VALUES(@idUser, 'Usuario promovido al rol de Tutor', GETDATE())
+
+			INSERT INTO proyecto1.HistoryLog(Date, Description)
+			VALUES(GETDATE(), 'OK - CambioRoles')
+
+			COMMIT TRAN RegistrarUsuarios
+
+	END TRY
+	BEGIN CATCH
+		ROLLBACK TRAN CambioRoles
+
+		INSERT INTO proyecto1.HistoryLog(Date, Description)
+		VALUES(GETDATE(),  CONCAT('ERROR - CambioRoles', ERROR_MESSAGE()))
+	END CATCH
+END
+GO
